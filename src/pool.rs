d1d073b7abb753799b407d892478dc47013ad854
Никита Кочнев <nikitakocnev@MacBook-Air-Nikita.local>
2023-01-02 03:47:07 +0300

    added drop

--- src/pool.rs
+++ src/pool.rs
@@ -1,11 +1,12 @@
 use std::sync::{Arc, mpsc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
-use std::thread::JoinHandle;
+use std::thread::{JoinHandle, sleep};
 use std::time::Duration;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 #[derive(Debug, Clone)]
@@ -13,7 +14,8 @@ pub struct PoolError;
 
 struct Worker {
     id: usize,
-    thread: JoinHandle<()>,
+    is_end: Arc<AtomicBool>,
+    thread: Option<JoinHandle<()>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -28,7 +30,7 @@ impl ThreadPool {
                 for i in 0..size {
                     workers.push(Worker::new(i, Arc::clone(&receiver)));
                 }
-                Ok(ThreadPool { workers, sender })
+                Ok(ThreadPool { workers, sender: Some(sender) })
             }
             false => Err(PoolError)
         }
@@ -37,19 +39,11 @@ impl ThreadPool {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let is_end = Arc::new(AtomicBool::new(false));
         Worker {
             id,
-            thread: thread::spawn(move || loop {
-                receiver
-                    .try_lock()
-                    .map(|x| x.try_recv())
-                    .map(|x| if let Ok(job) = x {
-                        println!("Worker-{} got a job", id);
-                        job()
-                    })
-                    .unwrap_or_default();
-                thread::sleep(Duration::from_millis(1));
-            }),
+            is_end: is_end.clone(),
+            thread: Some(create_thread(id, receiver, is_end.clone())),
         }
     }
 }
@@ -59,6 +53,38 @@ impl ThreadPool {
         where F: FnOnce() + Send + 'static
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        if let Some(s) = self.sender.as_ref() {
+            s.send(job).unwrap();
+        }
     }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        self.sender.take();
+        for worker in &mut self.workers {
+            if let Some(t) = worker.thread.take() {
+                worker.is_end.swap(true, Ordering::Relaxed);
+                t.join().unwrap();
+            }
+        }
+    }
+}
+
+fn create_thread(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>, is_end: Arc<AtomicBool>) -> JoinHandle<()> {
+    thread::spawn(move || loop {
+        if is_end.load(Ordering::Relaxed) {
+            println!("Worker-{} shutdown", id);
+            return;
+        }
+        receiver
+            .try_lock()
+            .map(|x| x.try_recv())
+            .map(|x| if let Ok(job) = x {
+                println!("Worker-{} got a job", id);
+                job()
+            })
+            .unwrap_or_default();
+        sleep(Duration::from_millis(1));
+    })
 }
